3a0be9d36f3fd4f43879c7094827cdc4bea73b49
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-07-07 16:36:07 -0300

    Fixes assertion

--- problem_001.py
+++ problem_001.py
@@ -1,6 +1,6 @@
 def get_sum_in_range(r):
     return sum(x for x in xrange(r) if not x % 3 or not x % 5)
 
-assert get_sum_in_range(10), 23
+assert get_sum_in_range(10) == 23
 
 print 'Result: %d' % get_sum_in_range(1000)
